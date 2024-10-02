c9713abfb5867e71cb6682cb1da51079d02f8fa9
村本祐樹 <mura@muramotoyuukinoMacBook-Air.local>
2024-10-03 00:04:36 +0900

    commit message

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,28 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+     public function tweets()
+    {
+        return $this->hasMany(Tweet::class);
+    }
+
+     public function likes()
+    {
+        return $this->belongsToMany(Tweet::class)->withTimestamps();
+    }
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    //'follows'以下は順番大切。（命名規則通りにすればいらない）
+    public function follows()
+    {
+        return $this->belongsToMany(User::class, 'follows', 'follow_id', 'follower_id');
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'follows', 'follower_id', 'follow_id');
+    }
 }
